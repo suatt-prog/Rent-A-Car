2320722915f258af878d53dce05c4a3d412af3d9
suatt-prog <72493647+suatt-prog@users.noreply.github.com>
2022-06-21 00:36:06 +0300

    Update kirala.php

--- kirala.php
+++ kirala.php
@@ -7,60 +7,61 @@
     </head>
     <body>
     <form action="kirala_back.php" method="post">
-        <div class="container">
-            <center>
-                <table border="1">
-                <tr>
-                    <td>
-                        Araç Modeli
-                    </td>
-                    <td>
-                        <input type="radio" name="arac" value="posat">
-                        posat ücret 300
-                        <input type="radio" name="arac" value="jetta">
-                        jetta ücret 250
-                        <input type="radio" name="arac" value="astra">
-                        astra ücret 200
-                        <input type="radio" name="arac" value="I20">
-                        I20 ücret 150
-                        <input type="radio" name="arac" value="fiorino">
-                        fiorino 170
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Tarih
-                    </td>
-                    <td>
-                        <input type="text" name="tarih" size="70">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Kaç gün kullanmak istiyorsunuz
-                    </td>
-                    <td>
-                        <input type="text" name="gun" size="70">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Teslim alınacak şube
-                    </td>
-                    <td>
-                        <input type="text" name="sube" size="70">
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                    </td>
-                    <td>
-                        <input type="submit" name="kayit" value="Kaydet">
-                    </td>
-                </tr>
-                </table>
-            </center>
-        </div>
+            <table border=1 class="table table-dark">
+            <thead>
+            <tr>
+                <td>
+                    Araç Modeli
+                </td>
+                <td>
+                    <input type="radio" name="arac" value="posat">
+                    posat ücret 300
+                    <input type="radio" name="arac" value="jetta">
+                    jetta ücret 250
+                    <input type="radio" name="arac" value="astra">
+                    astra ücret 200
+                    <input type="radio" name="arac" value="I20">
+                    I20 ücret 150
+                    <input type="radio" name="arac" value="fiorino">
+                    fiorino 170
+                </td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>
+                    Tarih
+                </td>
+                <td>
+                    <input type="text" name="tarih" size="70">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Kaç gün kullanmak istiyorsunuz
+                </td>
+                <td>
+                    <input type="text" name="gun" size="70">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Teslim alınacak şube
+                </td>
+                <td>
+                    <input type="text" name="sube" size="70">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                </td>
+                <td>
+                    <input type="submit" name="kayit" value="Kaydet">
+                </td>
+            </tr>
+            </tbody>
+            </table>
     </form>
     </body>
 </html>
+
